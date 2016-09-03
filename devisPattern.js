2d041c1400f8c6d62477bf02ec7ac2625f5b5d95
Ismail Reida <ismailrei@gmail.com>
2016-09-03 12:51:10 +0100

    Update devisPattern.js

--- devisPattern.js
+++ devisPattern.js
@@ -3,6 +3,7 @@ const jsonSort = require("sort-json");
 const obj = new addon.devisPattern();
 
 class devisPattern {
+
     constructor() {
         this.path = [];
         this.id = 0;
@@ -23,10 +24,15 @@ class devisPattern {
         if (id != -1) return this.path[id];
         else return null;
     }
-    
+    call(path,args,callback)
+    {
+      let res=this.find(path);
+      if(res) res.call(this,args,callback);
+    }
+
     list() {
         return obj.list();
     }
 
-};
+}
 module.exports = new devisPattern();
